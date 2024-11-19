d4c1fb819a634d9ee1ac4a4e5dbb83b361365608
Linwei Zhang <lewiszlw520@gmail.com>
2024-11-19 18:03:07 +0800

    Replace asm file with naked function

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,30 @@ mod console;
 mod lang_items;
 mod logging;
 
-use core::arch::{global_asm, naked_asm};
+use core::arch::{naked_asm};
 
 use log::info;
 
-global_asm!(include_str!("entry.asm"));
+const BOOT_STACK_SIZE: usize = 16 * 1000;
+
+#[link_section = ".bss.stack"]
+static BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0u8; BOOT_STACK_SIZE];
+
+#[link_section = ".text.entry"]
+#[export_name = "_start"]
+#[naked]
+pub unsafe extern "C" fn start() -> ! {
+    naked_asm!(
+        "la sp, {boot_stack}",
+        "li t2, {boot_stack_size}",
+        "addi t3, a0, 1",
+        "mul t2, t2, t3",
+        "add sp, sp, t2",
+        "call hmain",
+        boot_stack = sym BOOT_STACK,
+        boot_stack_size = const BOOT_STACK_SIZE,
+    )
+}
 
 /// clear BSS segment
 pub fn clear_bss() {
