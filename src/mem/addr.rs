9baa385bc06fab4bf81aa4094519376bd7846a2e
Linwei Zhang <lewiszlw520@gmail.com>
2024-11-22 10:35:16 +0800

    Use marco to generate common methods for addr

--- src/mem/addr.rs
+++ src/mem/addr.rs
@@ -37,6 +37,85 @@ pub const fn is_aligned(addr: usize, align: usize) -> bool {
     align_offset(addr, align) == 0
 }
 
+macro_rules! impl_common_addr_methods {
+    ($t: ty) => {
+        impl $t {
+            pub const fn new(addr: usize) -> Self {
+                Self(addr)
+            }
+
+            #[inline]
+            pub const fn as_usize(self) -> usize {
+                self.0
+            }
+
+            #[inline]
+            pub fn is_aligned<U>(self, align: U) -> bool
+            where
+                U: Into<usize>,
+            {
+                is_aligned(self.0, align.into())
+            }
+
+            #[inline]
+            pub fn align_up<U>(self, align: U) -> Self
+            where
+                U: Into<usize>,
+            {
+                Self(align_up(self.0, align.into()))
+            }
+
+            #[inline]
+            pub fn align_down<U>(self, align: U) -> Self
+            where
+                U: Into<usize>,
+            {
+                Self(align_down(self.0, align.into()))
+            }
+        }
+
+        impl From<usize> for $t {
+            #[inline]
+            fn from(addr: usize) -> Self {
+                Self(addr)
+            }
+        }
+
+        impl Add<usize> for $t {
+            type Output = Self;
+            #[inline]
+            fn add(self, rhs: usize) -> Self {
+                Self(self.0 + rhs)
+            }
+        }
+
+        impl AddAssign<usize> for $t {
+            #[inline]
+            fn add_assign(&mut self, rhs: usize) {
+                *self = *self + rhs;
+            }
+        }
+
+        impl Sub<usize> for $t {
+            type Output = Self;
+            #[inline]
+            fn sub(self, rhs: usize) -> Self {
+                Self(self.0 - rhs)
+            }
+        }
+
+        impl SubAssign<usize> for $t {
+            #[inline]
+            fn sub_assign(&mut self, rhs: usize) {
+                *self = *self - rhs;
+            }
+        }
+    };
+}
+
+impl_common_addr_methods!(PhysAddr);
+impl_common_addr_methods!(VirtAddr);
+
 /// A physical memory address.
 ///
 /// It's a wrapper type around an `usize`.
@@ -50,88 +129,6 @@ impl fmt::Debug for PhysAddr {
     }
 }
 
-impl PhysAddr {
-    pub const fn new(addr: usize) -> Self {
-        Self(addr)
-    }
-
-    /// Converts the address to an `usize`.
-    #[inline]
-    pub const fn as_usize(self) -> usize {
-        self.0
-    }
-
-    /// Checks whether the address has the demanded alignment.
-    ///
-    /// See the [`is_aligned`] function for more information.
-    #[inline]
-    pub fn is_aligned<U>(self, align: U) -> bool
-    where
-        U: Into<usize>,
-    {
-        is_aligned(self.0, align.into())
-    }
-
-    /// Aligns the address upwards to the given alignment.
-    ///
-    /// See the [`align_up`] function for more information.
-    #[inline]
-    pub fn align_up<U>(self, align: U) -> Self
-    where
-        U: Into<usize>,
-    {
-        Self(align_up(self.0, align.into()))
-    }
-
-    /// Aligns the address downwards to the given alignment.
-    ///
-    /// See the [`align_down`] function for more information.
-    #[inline]
-    pub fn align_down<U>(self, align: U) -> Self
-    where
-        U: Into<usize>,
-    {
-        Self(align_down(self.0, align.into()))
-    }
-}
-
-impl From<usize> for PhysAddr {
-    #[inline]
-    fn from(addr: usize) -> Self {
-        Self(addr)
-    }
-}
-
-impl Add<usize> for PhysAddr {
-    type Output = Self;
-    #[inline]
-    fn add(self, rhs: usize) -> Self {
-        Self(self.0 + rhs)
-    }
-}
-
-impl AddAssign<usize> for PhysAddr {
-    #[inline]
-    fn add_assign(&mut self, rhs: usize) {
-        *self = *self + rhs;
-    }
-}
-
-impl Sub<usize> for PhysAddr {
-    type Output = Self;
-    #[inline]
-    fn sub(self, rhs: usize) -> Self {
-        Self(self.0 - rhs)
-    }
-}
-
-impl SubAssign<usize> for PhysAddr {
-    #[inline]
-    fn sub_assign(&mut self, rhs: usize) {
-        *self = *self - rhs;
-    }
-}
-
 /// A virtual memory address.
 ///
 /// It's a wrapper type around an `usize`.
@@ -142,87 +139,5 @@ pub struct VirtAddr(usize);
 impl fmt::Debug for VirtAddr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_fmt(format_args!("VA:{:#x}", self.0))
-    }
-}
-
-impl VirtAddr {
-    pub const fn new(addr: usize) -> Self {
-        Self(addr)
-    }
-
-    /// Converts the address to an `usize`.
-    #[inline]
-    pub const fn as_usize(self) -> usize {
-        self.0
-    }
-
-    /// Checks whether the address has the demanded alignment.
-    ///
-    /// See the [`is_aligned`] function for more information.
-    #[inline]
-    pub fn is_aligned<U>(self, align: U) -> bool
-    where
-        U: Into<usize>,
-    {
-        is_aligned(self.0, align.into())
-    }
-
-    /// Aligns the address upwards to the given alignment.
-    ///
-    /// See the [`align_up`] function for more information.
-    #[inline]
-    pub fn align_up<U>(self, align: U) -> Self
-    where
-        U: Into<usize>,
-    {
-        Self(align_up(self.0, align.into()))
-    }
-
-    /// Aligns the address downwards to the given alignment.
-    ///
-    /// See the [`align_down`] function for more information.
-    #[inline]
-    pub fn align_down<U>(self, align: U) -> Self
-    where
-        U: Into<usize>,
-    {
-        Self(align_down(self.0, align.into()))
-    }
-}
-
-impl From<usize> for VirtAddr {
-    #[inline]
-    fn from(addr: usize) -> Self {
-        Self(addr)
-    }
-}
-
-impl Add<usize> for VirtAddr {
-    type Output = Self;
-    #[inline]
-    fn add(self, rhs: usize) -> Self {
-        Self(self.0 + rhs)
-    }
-}
-
-impl AddAssign<usize> for VirtAddr {
-    #[inline]
-    fn add_assign(&mut self, rhs: usize) {
-        *self = *self + rhs;
-    }
-}
-
-impl Sub<usize> for VirtAddr {
-    type Output = Self;
-    #[inline]
-    fn sub(self, rhs: usize) -> Self {
-        Self(self.0 - rhs)
-    }
-}
-
-impl SubAssign<usize> for VirtAddr {
-    #[inline]
-    fn sub_assign(&mut self, rhs: usize) {
-        *self = *self - rhs;
     }
 }
